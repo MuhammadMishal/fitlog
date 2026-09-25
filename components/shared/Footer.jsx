8e26e13f12381923f528c80da73be00c7d09c4ee
MuhammadMishal <muhammadmeshal01@gmail.com>
2026-09-25 19:14:26 +0600

    Initialize the structure add daisyUI and create navbar and banner section

---
+++ components/shared/Footer.jsx
@@ -0,0 +1,17 @@
+import footerLogo from "@/public/assets/logo.png";
+import React from "react";
+import Image from "next/image";
+
+export default function Footer() {
+  return (
+    <section className="flex justify-between p-5">
+      <div className="flex justify-between gap-2">
+        <Image src={footerLogo} alt="footer logo" />
+        <p className="font-extrabold">FITLOG</p>
+      </div>
+      <div className="text-gray-600">
+        © 2026 FitLog — Workout Library. Train hard, log honest.
+      </div>
+    </section>
+  );
+}
