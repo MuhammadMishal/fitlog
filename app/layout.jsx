d3b3a478a86d6a06f739d4d93965d4c8a516a251
MuhammadMishal <muhammadmeshal01@gmail.com>
2026-09-26 12:49:22 +0600

    change font to Inter

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,16 +3,22 @@ import Navbar from "@/components/shared/Navbar";
 import Footer from "@/components/shared/Footer";
 import WorkoutProvider from "@/context/WorkoutContext";
 import { ToastContainer } from "react-toastify";
+import { Inter } from "next/font/google";
 
 export const metadata = {
   title: "Fitlog",
   description: "Train with intent. Log every set.",
 };
 
+const guestFont = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="min-h-screen flex flex-col">
+      <body className={`min-h-screen flex flex-col ${guestFont.className}`}>
         <WorkoutProvider>
           <Navbar></Navbar>
 
--- app/page.jsx
+++ app/page.jsx
@@ -15,10 +15,6 @@ export default function Home() {
             href="https://fonts.gstatic.com"
             crossorigin
           ></link>
-          <link
-            href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap"
-            rel="stylesheet"
-          ></link>
         </>
       </Head>
     </>
